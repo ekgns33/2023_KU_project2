b33386af58983d9e63355534fd2c8c1e3440e32c
ekgns33 <ekgns00421@gmail.com>
2023-09-26 15:25:54 +0900

    create: Contact.java 클래스 프로퍼티 추가, 연락처 데이터 클래스를 의미.

--- src/contact/Contact.java
+++ src/contact/Contact.java
@@ -1,4 +1,57 @@
 package contact;
 
 public class Contact {
+    private int pid;
+    private String name;
+    private String phoneNumber;
+    private String groupName;
+    private String memo;
+
+    public Contact(int pid, String name, String phoneNumber, String groupName, String memo) {
+        this.pid = pid;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.groupName = groupName;
+        this.memo = memo;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
 }
