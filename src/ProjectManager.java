2b2fa6a171d0878e6f67c00b2463352c26d1d207
ekgns33 <ekgns00421@gmail.com>
2023-09-26 15:22:34 +0900

    create: ProjectManager.java 파일 init 메소드, bootProgram메소드 작성

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class ProjectManager {
+
+    public ProjectManager() {};
+
+    public void init() {
+
+    }
+    public void bootProgram() {
+        // read datas from FILE
+        init();
+        while(true) {
+            String userInput = getUserInput();
+            if(userInput.equals("0")) break;
+        }
+    }
+    public String getUserInput() {
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+        return userInput;
+    }
+
 }
