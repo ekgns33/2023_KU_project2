236ae2fedfd374ae9f92c1ddf303507af7832114
ekgns33 <ekgns00421@gmail.com>
2023-09-26 15:32:59 +0900

    create: ContactController.java 각각 요청을 서비스코드로 연결시키는 메소드들 추가

--- src/contact/ContactController.java
+++ src/contact/ContactController.java
@@ -1,4 +1,25 @@
 package contact;
 
 public class ContactController {
+    private ContactService contactService;
+    //constructor
+    public ContactController(){};
+
+    public void searchContact() {
+    }
+
+    public void createContact(){
+    }
+
+    public void updateContact() {
+
+    }
+    public void saveContact() {
+
+    }
+
+    public void deleteContact() {
+
+    }
+
 }
