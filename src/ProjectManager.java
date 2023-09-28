fdcef2ea0c4066f0c6220e0a4f7c7bd7b3755bfb
ekgns33 <ekgns00421@gmail.com>
2023-09-28 17:05:42 +0900

    update: ProjectManager.java 파일을 읽어 데이터를 레포지토리클래스의 맵에 저장하도록 코드 작성. ProjectManager클래스가 ProjectManagerSupport를 extend하여 여러 기능 사용

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -1,20 +1,25 @@
 import contact.ContactController;
 import contact.ContactRepository;
-
+import java.util.List;
 import java.util.Scanner;
 
-public class ProjectManager {
+public class ProjectManager extends ProjectManagerSupport {
 
     private final ContactController contactController;
     private final ContactRepository contactRepository;
 
     public ProjectManager() {
+        super("data.txt");
         // 의존성 부여를 위한 객체 생성
         this.contactController = new ContactController();
         this.contactRepository = new ContactRepository();
+
     }
 
     public void init() {
+
+        List<String> dataList = fileHandler.readFile();
+        this.contactRepository.setUserTable(contactMapper.mapListToHashMap(dataList));
 
     }
     public void bootProgram() {
@@ -22,8 +27,8 @@ public class ProjectManager {
         init();
         while(true) {
             String userInput = getUserInput();
+            if(userInput.equals("0")) break;
             this.contactController.routeRequest(Integer.parseInt(userInput));
-
         }
     }
     public String getUserInput() {
