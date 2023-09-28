c6bdf8eb759a365c060bd50bef488d5cdbcf9019
ekgns33 <ekgns00421@gmail.com>
2023-09-28 20:23:12 +0900

    update: ContactMapper.java 파일 java docs로 메소드 정보 표시

--- src/utils/ContactMapper.java
+++ src/utils/ContactMapper.java
@@ -10,6 +10,12 @@ public class ContactMapper {
 
     public ContactMapper() {}
 
+    /**
+     * file에서 읽은 문자열을 읽어서 Contact객체를 생성.
+     * @param lines 연락처 정보가 담긴 문자열 리스트
+     * @return Contact 객체들이 담긴 해시맵.
+     *
+     * */
     public Map<Integer, Contact> mapListToHashMap(List<String> lines) {
         Map<Integer, Contact> map = new HashMap<>();
 
@@ -19,6 +25,13 @@ public class ContactMapper {
         }
         return map;
     }
+
+    /**
+     * 문자열에서 Contact객체 정보를 분리하여 객체 생성.
+     * 그룹과 메모정보는 없으면 null로 저장
+     * @param line 연락처 정보가 담긴 문자열
+     * @return 문자열을 분해해서 만든 Contact 객체
+     * */
     public Contact mapStringToContact(String line) {
         String[] tokens = line.split(" ");
         Contact contact = new Contact(Integer.parseInt(tokens[0]), tokens[1], tokens[2]);
