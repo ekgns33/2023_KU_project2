98826f47c4bdaef8ab14631529b5764961fd7101
ParkSeongGeun <phd0801@naver.com>
2023-10-02 11:58:34 +0900

    searchService parameter로 userCommand, userTable 생성

--- src/contact/ContactService.java
+++ src/contact/ContactService.java
@@ -3,12 +3,14 @@ package contact;
 import java.util.Map;
 
 public class ContactService {
-
     public ContactService(){}
 
-    public void searchService(Integer userCommand, Map<Integer, Contact> userInfo){}
+    public void searchService(Integer userCommand, Map<Integer, Contact> userInfo){
+
+    }
     public void createService(){}
     public void deleteService(){}
     public void updateService(){}
     public void settingService(){}
+
 }
