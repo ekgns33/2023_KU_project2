b434eec01a1416a86485ce6667ca7f663926fca0
ParkSeongGeun <phd0801@naver.com>
2023-10-02 12:45:54 +0900

    contactService 내 정보 찾기 모델 구현

--- src/contact/ContactService.java
+++ src/contact/ContactService.java
@@ -5,23 +5,72 @@ import java.util.List;
 import java.util.Map;
 
 public class ContactService {
-    public ContactService() {
-    }
+    public ContactService(){}
 
-    public void searchService(Integer userCommand, Map<Integer, Contact> userInfo) {
+    public void searchService(Integer userCommand, Map<Integer, Contact> userInfo){
 
         findInfo(userCommand, userInfo);
     }
+    public void createService(){}
+    public void deleteService(){}
+    public void updateService(){}
+    public void settingService(){}
 
-    public void createService() {
+    public void findInfo(Integer userCommand, Map<Integer, Contact> userInfo){
+        String userInput;
+        // index., Contact 담을 arraylist 생성
+        List<IndexContact> matchContacts = new ArrayList<>();
+        // 인덱스
+        int index = 1;
+        System.out.println(index);
+        switch(userCommand){
+            case 1:
+                // 이름 입력받기 -> 예외 처리 X
+                for(Map.Entry<Integer, Contact> entry : userInfo.entrySet()){
+                    if(String.valueOf(entry.getValue().getName()).equals(userInput)){
+                        Contact contact = entry.getValue();
+                        matchContacts.add(new IndexContact(index++, contact));
+                    }
+                }
+            case 2:
+                // 전화번호 입력받기 -> 예외 처리 X
+                for(Map.Entry<Integer, Contact> entry : userInfo.entrySet()){
+                    if(String.valueOf(entry.getValue().getPhoneNumber()).equals(userInput)){
+                        Contact contact = entry.getValue();
+                        matchContacts.add(new IndexContact(index++, contact));
+                    }
+                }
+            case 3:
+                // 그룹 입력받기 -> 예외 처리 X
+                for(Map.Entry<Integer, Contact> entry : userInfo.entrySet()){
+                    if(String.valueOf(entry.getValue().getGroupName()).equals(userInput)){
+                        Contact contact = entry.getValue();
+                        matchContacts.add(new IndexContact(index++, contact));
+                    }
+                }
+        }
+        // 검색 결과 출력 또는 반환
+        if (!matchContacts.isEmpty()) {
+            for (IndexContact indexContact : matchContacts) {
+                System.out.println("인덱스: " + indexContact.getIndex() + ", Contact: " + indexContact.getContact());
+            }
+        } else {
+            System.out.println("검색 결과가 없습니다.");
+        }
     }
+}
 
-    public void deleteService() {
+class IndexContact{
+    private int index;
+    private Contact contact;
+    public IndexContact(int index, Contact contact){
+        this.index = index;
+        this.contact = contact;
     }
-
-    public void updateService() {
+    public int getIndex() {
+        return index;
     }
-
-    public void settingService() {
+    public Contact getContact() {
+        return contact;
     }
 }
