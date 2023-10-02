9530c15f8bc7013ad07732499953d43184718981
ekgns33 <ekgns00421@gmail.com>
2023-10-02 15:20:59 +0900

    update: ContactController.java 레포지토리 인스턴스를 ProjectManager로부터 받아 저장하도록 코드작성

--- src/contact/ContactController.java
+++ src/contact/ContactController.java
@@ -2,6 +2,7 @@ package contact;
 
 public class ContactController {
     private ContactService contactService;
+    private ContactRepository contactRepository;
 
     private int nextCommand;
     //constructor
@@ -27,8 +28,9 @@ public class ContactController {
             }
         }
     }
-    public ContactController(){
+    public ContactController(ContactRepository contactRepository){
         //initial value
+        this.contactRepository = contactRepository;
         this.nextCommand = -1;
     }
 
