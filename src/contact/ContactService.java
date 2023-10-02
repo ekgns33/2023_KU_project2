d0815acfd20b148a011f1f840a13bae11e6f9571
ParkSeongGeun <phd0801@naver.com>
2023-10-03 01:11:06 +0900

    switch break추가 및 queryResult not empty -> 출력

--- src/contact/ContactService.java
+++ src/contact/ContactService.java
@@ -14,18 +14,26 @@ public class ContactService {
             case 1:
                 String inputName = getUserInput();
                 queryResult = contactRepository.findByName(inputName);
+                break;
             case 2:
                 String inputPhoneNumber = getUserInput();
                 queryResult = contactRepository.findByPhoneNumber(inputPhoneNumber);
+                break;
             case 3:
                 String inputGroup = getUserInput();
                 queryResult = contactRepository.findByGroupName(inputGroup);
+                break;
             default:
                 break;
         }
         if(queryResult.isEmpty()){
             System.out.println("일치하는 항목이 없습니다.");
             return ;
+        }
+        else{
+            for(Contact contact : queryResult){
+                System.out.println(queryResult);
+            }
         }
     }
     public String getUserInput() {
