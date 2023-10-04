c39705b5d736b08cf550ce0a0220eb743f4e90ce
ekgns33 <ekgns00421@gmail.com>
2023-10-04 17:24:49 +0900

    update: ContactService.java update 메소드 추가

--- src/contact/ContactService.java
+++ src/contact/ContactService.java
@@ -68,6 +68,84 @@ public class ContactService {
         }
         return null;
     }
+    public void update(int userInput, ContactRepository contactRepository) {
+        // 검색기능 그대로 사용한다.
+        List<Contact> queryResult = searchByInputType(userInput, contactRepository);
+        if (queryResult.isEmpty()) {
+            System.out.println("일치하는 항목이 없습니다.");
+        } else {
+            showContactList(queryResult);
+            int targetIndex = selectIndex();
+            if (targetIndex == 0) return; // detail을 안보겠다는 의미 -> 수정을 할 수가 없음 그냥 리턴한다.
+
+            Contact selectedContact = queryResult.get(targetIndex - 1); // 0-based index;
+            System.out.print("수정하시겠습니까?(Y/N)\n>> ");
+            String updateDecision = getUserInput();
+            if (updateDecision.equals("N")) return;
+            Contact updateContact = new Contact(selectedContact.getPid());
+            while(true) {
+                try {
+                    System.out.print("이름을 입력해주세요 (" + selectedContact.getName() + ") :");
+                    String inputName = getUserInput();
+                    //validate
+                    if(inputName.equals("")) {
+                        updateContact.setName(selectedContact.getName());
+                        break;
+                    }
+                    updateContact.setName(inputName);
+                    break;
+                } catch (ApplicationException e){
+                    System.out.print("다시 입력해주세요 : ");
+                }
+            }
+            while(true) {
+                try {
+                    System.out.print("전화번호을 입력해주세요 (" + selectedContact.getPhoneNumber() + ") :");
+                    String inputPhoneNumber = getUserInput();
+                    //validate
+                    if(inputPhoneNumber.equals("")) {
+                        updateContact.setPhoneNumber(selectedContact.getPhoneNumber());
+                        break;
+                    }
+
+                    updateContact.setPhoneNumber(inputPhoneNumber);
+                    break;
+                } catch (ApplicationException e){
+                    System.out.print("다시 입력해주세요 : ");
+                }
+            }
+            while(true) {
+                try {
+                    System.out.print("그룹명을 입력해주세요 (" + selectedContact.getGroupName() + ") :");
+                    String inputGroupName = getUserInput();
+                    //validate
+                    if(inputGroupName.equals("")) {
+                        updateContact.setGroupName(selectedContact.getGroupName());
+                        break;
+                    }
+                    updateContact.setGroupName(inputGroupName);
+                    break;
+                } catch (ApplicationException e){
+                    System.out.print("다시 입력해주세요 : ");
+                }
+            }
+            System.out.print("메모를 입력해주세요 (" + selectedContact.getMemo() + ") : ");
+            String inputMemo = getUserInput();
+            updateContact.setMemo(inputMemo);
+            System.out.println("==========");
+            System.out.println(updateContact.toString());
+            System.out.println("==========");
+            System.out.print("저장하시겠습니까? (Y or N) : ");
+            String saveDecision = getUserInput();
+            if(saveDecision.equals("Y")) {
+                contactRepository.updateContact(updateContact);
+            } else {
+                System.out.println("부적절한 입력으로 수정내용 저장이 취소되었습니다.");
+                return;
+            }
+
+        }
+    }
     public int selectIndex(){
         System.out.print("인덱스 선택 : ");
         String userInput = getUserInput();
