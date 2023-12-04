6cd8e45211943a6c4344df692b0c1799f6da2e18
ekgns33 <ekgns00421@gmail.com>
2023-12-05 00:24:02 +0900

    feature: 파일에서 읽을때 매핑테이블 구성하는 로직 추가, 주석 제거

--- src/ProjectManager.java
+++ src/ProjectManager.java
@@ -45,9 +45,13 @@ public class ProjectManager extends ProjectManagerSupport {
                 System.out.println("그룹 정보 파일 형식에 오류가 있습니다.");
                 System.exit(0);
             }
-            if (groupInfo.size() == 1) {
-                this.contactRepository.setGroupTable(contactMapper.groupInfoToArrayList(groupInfo));
+            if (groupInfo.isEmpty()) {
+                System.out.println("그룹 정보 파일 형식에 오류가 있습니다.");
+                System.exit(0);
             }
+            List<String> groupList = contactMapper.groupInfoToArrayList(groupInfo);
+            this.contactRepository.initGroupMap(groupList);
+            this.contactRepository.setGroupTable(new HashSet<>(groupList));
 
             // phonebook.txt 내용 불러오기
             List<String> dataList = fileHandler.readFile("src/phonebook.txt");
